ce81f847b810d8c5c2e408bab6064065dd3d74b6
joseLozanoTamayo <jose.lozano.tamayo@gmail.com>
2019-02-13 10:25:06 -0500

    implementacion de test unit y solve issues

--- back-adn/ceiba-rest/src/main/java/org/adn/ceiba/ceibarest/CeibaRestApplication.java
+++ back-adn/ceiba-rest/src/main/java/org/adn/ceiba/ceibarest/CeibaRestApplication.java
@@ -1,17 +1,11 @@
 package org.adn.ceiba.ceibarest;
 
 
-import org.adn.ceiba.ceibarest.entity.TipoVehiculo;
-import org.adn.ceiba.ceibarest.repository.TipoVehiculoRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 /**
  * Clase init de spring boot
@@ -22,8 +16,6 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
 public class CeibaRestApplication extends SpringBootServletInitializer {
 	
-	private static final Logger log = LoggerFactory.getLogger(CeibaRestApplication.class);
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CeibaRestApplication.class);
@@ -32,34 +24,5 @@ public class CeibaRestApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(CeibaRestApplication.class, args);
 	}
-	
-//	@Bean
-//	public CommandLineRunner demo (TipoVehiculoRepository repository) {
-//		return args -> {
-//
-//			TipoVehiculo v = new TipoVehiculo();
-//			v.setVehiculo("carro");
-//			
-//			repository.save(v);
-//
-//			// fetch all customers
-//			log.info("Customers found with findAll():");
-//			log.info("-------------------------------");
-//			log.info("");
-//
-//			// fetch an individual customer by ID
-//			repository.findById(1)
-//					.ifPresent(customer -> {
-//						log.info("Customer found with findById(1L):");
-//						log.info("--------------------------------");
-//						log.info(customer.toString());
-//						log.info("");
-//					});
-//
-//			// fetch customers by last name
-//			log.info("Customer found with findByLastName('Bauer'):");
-//			log.info("");
-//		};
-//	}
 
 }
